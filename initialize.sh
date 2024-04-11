26136ca9646c8396e3e90c0e3e9676686b9d7b04
藤本 健太 <kenta.fujimoto.2020@gmail.com>
2024-04-11 20:47:42 +0900

    initialize.shがどのディレクトリから呼ばれても正常にパスを通せるよう修正

--- initialize.sh
+++ initialize.sh
@@ -7,13 +7,13 @@ if [ "$SHELL_NAME" = "bash" ]; then
     # bash_profile新規作成時もしくはbash_profileにすでにパス登録されていない場合のみ、当ディレクトリをパス登録
     if [ ! -e ~/.bash_profile ] || ! grep -q "$PRJ_DIR_NAME" ~/.bash_profile ; then
         echo "" >> ~/.bash_profile
-        echo "export PATH=\"\$PATH:$(pwd)\"" >> ~/.bash_profile
+        echo "export PATH=\"\$PATH:$(cd $(dirname $0);pwd)\"" >> ~/.bash_profile
     fi
 elif [ "$SHELL_NAME" = "zsh" ]; then
     # zshrc新規作成時もしくはzshrcにすでにパス登録されていない場合のみ、当ディレクトリをパス登録
     if [ ! -e ~/.zshrc ] ||  ! grep -q "$PRJ_DIR_NAME" ~/.zshrc ; then
         echo "" >> ~/.zshrc
-        echo "export PATH=\"\$PATH:$(pwd)\"" >> ~/.zshrc
+        echo "export PATH=\"\$PATH:$(cd $(dirname $0);pwd)\"" >> ~/.zshrc
     fi
 fi
 
